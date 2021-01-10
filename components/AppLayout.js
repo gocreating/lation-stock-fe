a7ad283eb7748c64a6cd4657c1071888d4e249cb
gocreating <gocreating@gmail.com>
2021-01-10 22:28:18 +0800

    Improve SEO

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import Head from 'next/head'
 import Container from 'react-bootstrap/Container'
 import { Trans } from 'react-i18next'
@@ -16,7 +17,13 @@ const AppLayout = ({ t, title, titleSuffix, noContainer, children }) => {
       <Head>
         <title>{`${title}${titleSuffix}`}</title>
         <link rel="icon" href="/logo.png" />
+        <link rel="alternate" href="https://stock.lation.app/en" hrefLang="en" />
+        <link rel="alternate" href="https://stock.lation.app/zh-TW" hrefLang="zh-tw" />
       </Head>
+      <NextSeo
+        title={t('site.title')}
+        description={t('site.description')}
+      />
       <GoogleAnalyticsHeader />
       <FacebookMessengerHeader />
       <Navigation />
